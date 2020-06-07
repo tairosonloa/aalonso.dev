a602c3737a3d46b949df1ccc014620ab38d89d8d
Aitor Alonso <tairosonloa@gmail.com>
2020-06-07 22:31:25 +0200

    chore: templates and typescript changes

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,3 @@
-// / <reference types="next" />
-// / <reference types="next/types/global" />
+/* eslint-disable spaced-comment */
+/// <reference types="next" />
+/// <reference types="next/types/global" />
