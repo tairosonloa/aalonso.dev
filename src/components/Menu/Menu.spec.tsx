32f10ea9d8855a3efcab48c8a2389ea39b5ebcdd
Aitor Alonso <aitor@aalonso.dev>
2020-10-08 10:50:46 +0200

    test: updates test and storybooks

--- src/components/Menu/Menu.spec.tsx
+++ src/components/Menu/Menu.spec.tsx
@@ -1,17 +1,34 @@
-import { render } from '@testing-library/react'
+import { act, fireEvent, render } from '@testing-library/react'
 import * as React from 'react'
+import { RouterFake } from '../../services/router/RouterFake'
+import { Routes } from '../../services/router/Routes'
 import { Menu } from './Menu'
 
+const pushSpy = jest.fn()
+const routerSpy = new RouterFake({ push: pushSpy })
 const links = [
   { href: '#bio', text: 'About me' },
   { href: '#skills', text: 'Skills' },
   { href: '#career', text: 'Career' },
 ]
 
+const linksRoutes = [{ href: Routes.home(), text: 'Home' }]
+
 describe('<Header/>', () => {
   it.each(links)('should display the links to $href with text $text', ({ href, text }) => {
-    const renderResult = render(<Menu links={links} />)
+    const renderResult = render(<Menu links={links} router={routerSpy} />)
     expect(renderResult.getByText(text)).toBeTruthy()
     expect(renderResult.getByTestId(text)).toHaveAttribute('href', href)
+  })
+
+  it.each(linksRoutes)('should display the $text link', async ({ href, text }) => {
+    const renderResult = render(<Menu links={linksRoutes} router={routerSpy} />)
+    const link = renderResult.getByTestId(text)
+    expect(renderResult.getByText(text)).toBeTruthy()
+    expect(link).toHaveAttribute('href', href.getPath())
+    await act(async () => {
+      fireEvent.click(link)
+    })
+    expect(pushSpy).toHaveBeenCalledWith(href.getPath())
   })
 })
--- src/containers/Header/Header.spec.tsx
+++ src/containers/Header/Header.spec.tsx
@@ -1,7 +1,9 @@
 import { render } from '@testing-library/react'
 import * as React from 'react'
+import { RouterFake } from '../../services/router/RouterFake'
 import { Header } from './Header'
 
+const routerFake = new RouterFake()
 const headerLinks = [
   { href: '#bio', text: 'About me' },
   { href: '#skills', text: 'Skills' },
@@ -10,7 +12,7 @@ const headerLinks = [
 
 describe('<Header/>', () => {
   it('should match the snapshot', () => {
-    const renderResult = render(<Header links={headerLinks} />)
+    const renderResult = render(<Header links={headerLinks} router={routerFake} />)
     expect(renderResult).toMatchSnapshot()
   })
 })
