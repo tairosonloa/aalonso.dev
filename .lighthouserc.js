0d4079a2cadfa95cdd643b34499683d7ae7ce199
Aitor Alonso <aitor@aalonso.dev>
2020-11-01 02:36:25 +0100

    chore: remove ignored lighthouse asserts after resolving next/image bug

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -5,7 +5,7 @@ module.exports = {
       assertions: {
         'categories:performance': ['error', { minScore: 0.8 }],
         'categories:accessibility': ['error', { minScore: 0.95 }],
-        'categories:best-practices': ['error', { minScore: 0.9 }],
+        'categories:best-practices': ['error', { minScore: 1 }],
         'categories:seo': ['error', { minScore: 1 }],
         'first-contentful-paint': ['error', { maxNumericValue: 2500 }],
         'largest-contentful-paint': ['error', { maxNumericValue: 5000 }],
@@ -19,7 +19,6 @@ module.exports = {
         'unused-css-rules': ['warn', { maxNumericValue: 0 }],
         viewport: 'error',
         'color-contrast': 'off',
-        'errors-in-console': 'off',
         'font-display': 'off',
         'unsized-images': 'off',
         'uses-rel-preconnect': 'off',
