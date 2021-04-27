0f42f0bcd228dfe77195c2263f52d66d81680033
Aitor Alonso <aitor@aalonso.dev>
2021-04-27 22:59:21 +0100

    refactor: cleans header and mobile menu code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,11 @@ import React, { FC, useState } from 'react'
 
 const Header: FC = () => {
   const [isOpen, setIsOpen] = useState(false)
+  const navMenu = [
+    { text: 'Home', page: '/' },
+    { text: 'Blog', page: '/blog' },
+    { text: 'About', page: '/about' },
+  ]
 
   return (
     <header className="fixed top-0 w-full z-20">
@@ -27,107 +32,77 @@ const Header: FC = () => {
               </div>
               <div className="hidden md:block">
                 <div className="ml-10 flex items-baseline space-x-4">
-                  <Link href="/">
-                    <a className="hover:bg-gray-800 focus:bg-gray-800 px-3 py-2 rounded-md text-sm lg:text-base font-medium">
-                      Home
-                    </a>
-                  </Link>
-                  <Link href="/blog">
-                    <a className="hover:bg-gray-800 focus:bg-gray-800 px-3 py-2 rounded-md text-sm lg:text-base font-medium">
-                      Blog
-                    </a>
-                  </Link>
-                  <Link href="/about">
-                    <a className="hover:bg-gray-800 focus:bg-gray-800 px-3 py-2 rounded-md text-sm lg:text-base font-medium">
-                      About
-                    </a>
-                  </Link>
+                  {navMenu.map((item) => (
+                    <Link href={item.page} key={item.text}>
+                      <a className="hover:bg-gray-800 focus:bg-gray-800 px-3 py-2 rounded-md text-sm lg:text-base font-medium">
+                        {item.text}
+                      </a>
+                    </Link>
+                  ))}
                 </div>
               </div>
             </div>
+
+            {/* <!-- Mobile menu button --> */}
             <div className="-mr-2 flex md:hidden">
-              {/* <!-- Mobile menu button --> */}
               <button
                 onClick={() => setIsOpen(!isOpen)}
                 type="button"
-                className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-300 hover:text-gray-100 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-300 hover:text-gray-100 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-transparent"
                 aria-controls="mobile-menu"
                 aria-expanded="false"
               >
                 <span className="sr-only">Open main menu</span>
-                {!isOpen ? (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  {!isOpen ? (
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
                       strokeWidth="2"
                       d="M4 6h16M4 12h16M4 18h16"
                     />
-                  </svg>
-                ) : (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
+                  ) : (
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
                       strokeWidth="2"
                       d="M6 18L18 6M6 6l12 12"
                     />
-                  </svg>
-                )}
+                  )}
+                </svg>
               </button>
             </div>
           </div>
         </div>
 
+        {/* <!-- Mobile menu --> */}
         <Transition
           show={isOpen}
-          enter="ease-out duration-100 transform"
+          enter="linear duration-100 transform"
           enterFrom="opacity-0 scale-95"
           enterTo="opacity-100 scale-100"
-          leave="ease-in-out duration-100 transform"
+          leave="linear duration-100 transform"
           leaveFrom="opacity-100 scale-100"
           leaveTo="opacity-0 scale-95"
         >
-          {/* <!-- Mobile menu, show/hide based on menu state. --> */}
-          {(ref) => (
-            <div className="border-b border-gray-600 md:hidden" id="mobile-menu">
-              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                <div onClick={() => setIsOpen(!isOpen)} aria-hidden="true">
-                  <Link href="/">
-                    <a className="hover:bg-gray-800 focus:bg-gray-800 block px-3 py-2 rounded-md text-sm md:text-base font-medium">
-                      Home
+          {() => (
+            <div className="h-screen flex flex-col items-center pt-14 md:hidden" id="mobile-menu">
+              {navMenu.map((item) => (
+                <div onClick={() => setIsOpen(!isOpen)} aria-hidden="true" key={item.text}>
+                  <Link href={item.page}>
+                    <a className="hover:bg-gray-800 focus:bg-gray-800 block px-3 py-2 rounded-md text-xl font-medium mb-6">
+                      {item.text}
                     </a>
                   </Link>
                 </div>
-                <div onClick={() => setIsOpen(!isOpen)} aria-hidden="true">
-                  <Link href="/blog">
-                    <a className="hover:bg-gray-800 focus:bg-gray-800 block px-3 py-2 rounded-md text-sm md:text-base font-medium">
-                      Blog
-                    </a>
-                  </Link>
-                </div>
-                <div onClick={() => setIsOpen(!isOpen)} aria-hidden="true">
-                  <Link href="/about">
-                    <a className="hover:bg-gray-800 focus:bg-gray-800 block px-3 py-2 rounded-md text-sm md:text-base font-medium">
-                      About
-                    </a>
-                  </Link>
-                </div>
-              </div>
+              ))}
             </div>
           )}
         </Transition>
